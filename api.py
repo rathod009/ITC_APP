a9da66a3d565d7fe0998bad3f6abe30c58edef48
Bhakti joshi <87695740+bhakti1307@users.noreply.github.com>
2025-03-12 18:11:09 +0530

    Initial commit

--- api.py
+++ api.py
@@ -162,4 +162,4 @@ def submit_inquiry():
             connection.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1433)
+    app.run(debug=True, host='0.0.0.0', port=5000)
